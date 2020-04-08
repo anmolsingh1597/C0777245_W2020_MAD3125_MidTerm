8cf6a468b885d3ba1f148f25d3cb2d88b13e3835
Anmol Singh <anmolsingh1597@gmail.com>
2020-04-08 09:14:18 -0400

    [Anmol Singh]PersonEntryInfo - Date fetched from DatePicer

--- app/src/main/java/com/lambton/c0777245_w2020_mad3125_midterm/PersonInformationEntryActivity.java
+++ app/src/main/java/com/lambton/c0777245_w2020_mad3125_midterm/PersonInformationEntryActivity.java
@@ -2,11 +2,16 @@ package com.lambton.c0777245_w2020_mad3125_midterm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class PersonInformationEntryActivity extends AppCompatActivity {
 
@@ -15,12 +20,14 @@ public class PersonInformationEntryActivity extends AppCompatActivity {
     private TextView firstNameText;
     private TextView lastNameText;
     private Spinner genderSpinner;
+    private TextView dateOfBirthText;
 
     long sin;
     String suffixTitle;
     String firstName;
     String lastName;
     String gender;
+    Date dateOfBirth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +41,7 @@ public class PersonInformationEntryActivity extends AppCompatActivity {
         firstNameText = findViewById(R.id.enterFirstNameTextView);
         lastNameText = findViewById(R.id.enterLastNameTextView);
         genderSpinner = findViewById(R.id.genderCodeSpinner);
+        dateOfBirthText = findViewById(R.id.enterDateTextView);
     }
 
     private void suffix(){
@@ -59,7 +67,26 @@ public class PersonInformationEntryActivity extends AppCompatActivity {
     }
 
     public void dateImageButton(View view){
-        Toast.makeText(PersonInformationEntryActivity.this, "Date Picker", Toast.LENGTH_SHORT).show();
+        DatePickerDialog datePickerDialog;
+        final Calendar c = Calendar.getInstance();
+        int mYear = c.get(Calendar.YEAR); // current year
+        int mMonth = c.get(Calendar.MONTH); // current month
+        int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
+// date picker dialog
+        datePickerDialog = new DatePickerDialog(PersonInformationEntryActivity.this,
+                new DatePickerDialog.OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker view, int year,
+                                          int monthOfYear, int dayOfMonth) {
+                        // set day of month , month and year value in the edit text
+                        dateOfBirthText.setText(dayOfMonth + "/"
+                                + (monthOfYear + 1) + "/" + year);
+//                        dateOfBirth = (Date)String.valueOf(dateOfBirthText);
+
+                    }
+                }, mYear, mMonth, mDay);
+        datePickerDialog.show();
     }
 
     public void save(View view){
