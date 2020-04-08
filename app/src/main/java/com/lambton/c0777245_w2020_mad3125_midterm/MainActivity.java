9f4d941ab5af97e806950fb04018fbfeaa42c13f
Anmol Singh <anmolsingh1597@gmail.com>
2020-04-07 23:12:55 -0400

    Initial commit

---
+++ app/src/main/java/com/lambton/c0777245_w2020_mad3125_midterm/MainActivity.java
@@ -0,0 +1,14 @@
+package com.lambton.c0777245_w2020_mad3125_midterm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
