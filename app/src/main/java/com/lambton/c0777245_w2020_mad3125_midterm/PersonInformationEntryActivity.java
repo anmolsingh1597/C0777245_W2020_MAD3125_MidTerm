3b1253100707967ef490301333c39d0583dcfa3e
Anmol Singh <anmolsingh1597@gmail.com>
2020-04-07 23:29:25 -0400

    [Anmol Singh]PersonEntryInfo - Layout designed

--- app/src/main/java/com/lambton/c0777245_w2020_mad3125_midterm/PersonInformationEntryActivity.java
+++ app/src/main/java/com/lambton/c0777245_w2020_mad3125_midterm/PersonInformationEntryActivity.java
@@ -3,6 +3,8 @@ package com.lambton.c0777245_w2020_mad3125_midterm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class PersonInformationEntryActivity extends AppCompatActivity {
 
@@ -11,4 +13,15 @@ public class PersonInformationEntryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void dateImageButton(View view){
+        Toast.makeText(PersonInformationEntryActivity.this, "Date Picker", Toast.LENGTH_SHORT).show();
+    }
+
+    public void save(View view){
+        Toast.makeText(PersonInformationEntryActivity.this, "Save", Toast.LENGTH_SHORT).show();
+    }
+    public void clear(View view){
+        Toast.makeText(PersonInformationEntryActivity.this, "Clear", Toast.LENGTH_SHORT).show();
+    }
 }
+
