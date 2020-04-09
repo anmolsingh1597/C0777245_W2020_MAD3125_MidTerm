7d746e3906630ce22ce643c4f348ce5d77638bd7
Anmol Singh <anmolsingh1597@gmail.com>
2020-04-09 15:42:52 -0400

    [Anmol Singh]Tax Filing - Splash activity generated and designed

--- app/src/main/java/com/lambton/c0777245_w2020_mad3125_midterm/SplashActivity.java
+++ app/src/main/java/com/lambton/c0777245_w2020_mad3125_midterm/SplashActivity.java
@@ -2,13 +2,30 @@ package com.lambton.c0777245_w2020_mad3125_midterm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private static int SPLASH_SCREEN_TIME_OUT=5000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(SplashActivity.this,
+                        PersonInformationEntryActivity.class);
+                //Intent is used to switch from one activity to another.
+
+                startActivity(i);
+                //invoke the SecondActivity.
+
+                finish();
+                //the current activity will get finished.
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
     }
 }
