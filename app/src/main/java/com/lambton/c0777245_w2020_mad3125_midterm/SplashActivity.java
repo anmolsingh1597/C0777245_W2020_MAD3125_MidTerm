3015cc72788ef791fabcfdfdb5d6313965b1b94b
Anmol Singh <anmolsingh1597@gmail.com>
2020-04-09 15:37:29 -0400

    [Anmol Singh]Tax Filing - Splash activity generated

---
+++ app/src/main/java/com/lambton/c0777245_w2020_mad3125_midterm/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.lambton.c0777245_w2020_mad3125_midterm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
